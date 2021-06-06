8b7fa2afd4f0886b3c43b1df1baaf7eb0be503c1
alex.hartstone@gmail.com <alex.hartstone@gmail.com>
2021-06-06 13:59:39 +1000

    dev: using egui for simple context menu example

--- src/space.rs
+++ src/space.rs
@@ -69,7 +69,8 @@ fn setup(
                             })
                         }
                     ]
-                });
+                })
+                .insert(Popup);
             })),
             ..Default::default()
         })
