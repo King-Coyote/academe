9a34c2b1952405a117edbf6484702e596c48e97a
alex.hartstone@gmail.com <alex.hartstone@gmail.com>
2021-06-14 18:09:18 +1000

    dev: added simple ortho area and loading it on startup

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,27 @@ mod game;
 // .add_stage(stage::POST_UPDATE)
 // .add_stage(stage::LAST)
 
+fn area_texture_test(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    assets: Res<AssetServer>,
+) {
+    let tex_handle: Handle<Texture> = assets.load("textures/render.png");
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(tex_handle.into()),
+        ..Default::default()
+        // pub sprite: Sprite,
+        // pub mesh: Handle<Mesh>, // TODO: maybe abstract this out
+        // pub material: Handle<ColorMaterial>,
+        // pub main_pass: MainPass,
+        // pub draw: Draw,
+        // pub visible: Visible,
+        // pub render_pipelines: RenderPipelines,
+        // pub transform: Transform,
+        // pub global_transform: GlobalTransform,
+    });
+}
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
@@ -32,5 +53,6 @@ fn main() {
         .add_plugin(SpacePlugin)
         .add_plugin(UiPlugin)
         .add_plugin(GamePlugin)
+        .add_startup_system(area_texture_test.system())
         .run();
 }
